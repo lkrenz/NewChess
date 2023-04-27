3ed36934c4ab8134272e0439a9d2c6e6ddd03a27
liamk <liam.krenz@menloschool.org>
2023-04-26 21:19:39 -0700

    Fixed problem with cloning pieces and created instance variables in each piece to store the board. Started to code how each side will make their moves.

--- src/Bishop.java
+++ src/Bishop.java
@@ -17,16 +17,52 @@ public class Bishop extends Piece{
         return false;
     }
 
+    // Adds all possible moves to new arraylist and returns it to super.
     public void findMoves() {
+        ArrayList<Location> moves = new ArrayList<>();
         int row = getRow();
         int col = getCol();
-        ArrayList<Location> moves = new ArrayList<>();
-
-
+        addMoves(moves, 1, 1, row, col);
+        addMoves(moves, 1, -1, row, col);
+        addMoves(moves, -1, 1, row, col);
+        addMoves(moves, -1, -1, row, col);
+        setMoves(moves);
     }
 
-    public ArrayList<Location> addMoves(int dRow, int dCol, int row, int col) {
-        if (row = 7)
+    // Recursively moves diagonally until hits edge of board or another piece, adding each location to moves.
+    public void addMoves(ArrayList<Location> moves, int dRow, int dCol, int row, int col) {
+        if (getBoard().getBoard()[row][col].hasPiece()) {
+            if (getBoard().canMove(getColor(), row, col)) {
+                moves.add(new Location(row, col));
+            }
+            return;
+        }
+        if (dRow > 0) {
+            if (row == 7) {
+                moves.add(new Location(row, col));
+                return;
+            }
+        }
+        else {
+            if (row == 0) {
+                moves.add(new Location(row, col));
+                return;
+            }
+        }
+        if (dCol > 0) {
+            if (col == 7) {
+                moves.add(new Location(row, col));
+                return;
+            }
+        }
+        else {
+            if (col == 0) {
+                moves.add(new Location(row, col));
+                return;
+            }
+        }
+        moves.add(new Location(row, col));
+        addMoves(moves, dRow, dCol, row + dRow, col + dCol);
     }
 
     public Bishop clone(Board board) {
--- src/King.java
+++ src/King.java
@@ -15,8 +15,57 @@ public class King extends Piece{
     public ArrayList<Location> findControlled() {
         return null;
     }
-
     public King clone(Board board) {
         return new King(getRow(), getCol(), getColor(), board);
+    }
+
+    // Adds all possible king moves then removes any that are controlled by the opposite color
+    public void findMoves() {
+        int row = getRow();
+        int col = getCol();
+
+        ArrayList<Location> moves = new ArrayList<>();
+        // Checks to the right and right down
+        if (row < 7) {
+            moves.add(new Location(row + 1, col));
+            if (col < 7) {
+                moves.add( new Location(row + 1, col + 1));
+            }
+            if (col > 0) {
+                moves.add(new Location(row + 1, col - 1));
+            }
+        }
+        // Checks left and left up
+        if (row > 0) {
+            moves.add(new Location(row - 1, col));
+            if (col > 0) {
+                moves.add(new Location(row - 1, col - 1));
+            }
+            if (col < 7) {
+                moves.add(new Location(row - 1, col + 1));
+            }
+        }
+        if (col < 7) {
+            moves.add(new Location(row, col + 1));
+        }
+        if (col > 0) {
+            moves.add(new Location(row, col - 1));
+        }
+        ArrayList<Location> checkArr;
+        if (getColor() == 1) {
+            checkArr = getBoard().getBlackControls();
+        }
+        else {
+            checkArr = getBoard().getWhiteControls();
+        }
+        int i = 0;
+        while (i < moves.size()) {
+            if (checkArr.contains(moves.get(i))) {
+                moves.remove(i);
+                continue;
+            }
+            i++;
+        }
+        setMoves(moves);
     }
 }
